42d66ac780dc18bd58f3b753f6278f7a0dc21a86
manurare <mreyarea@gmail.com>
2019-03-12 20:54:37 +0100

    Add colorspace to adaptive method

--- code/week2/adap_bg_estimation.py
+++ code/week2/adap_bg_estimation.py
@@ -9,7 +9,7 @@ from utils.metrics import evaluation_detections, compute_map
 from utils.plot import plot_pr, show_bboxes
 
 
-def adaptive_bg_estimation(path, alpha=2.5, rho=0.1, mask_roi=None, path_to_save=None):
+def adaptive_bg_estimation(path, alpha=2.5, rho=0.1, mask_roi=None, path_to_save=None, colorspace='GRAY'):
     capture = cv2.VideoCapture(path)
     totalFrames = capture.get(cv2.CAP_PROP_FRAME_COUNT)
     width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -21,29 +21,43 @@ def adaptive_bg_estimation(path, alpha=2.5, rho=0.1, mask_roi=None, path_to_save
 
     # Load training sequence
     trainingFrames = totalFrames * 0.25
-    frames_acc = np.zeros((height, width), dtype='float')
+    if colorspace == 'GRAY':
+        frames_acc = np.zeros((height, width), dtype='float')
+        var_acc = np.zeros((height, width), dtype='float')
+    elif colorspace == 'YUV':
+        frames_acc = np.zeros((height, width, 3), dtype='float')
+        var_acc = np.zeros((height, width, 3), dtype='float')
+    else:
+        print("wrong color space")
+        return
+
     currentFrame = 0
     while success and currentFrame < trainingFrames:
         success, frame = capture.read()
         currentFrame = int(capture.get(cv2.CAP_PROP_POS_FRAMES))
-        grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        frames_acc += grayFrame
+        if colorspace == 'GRAY':
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        elif colorspace == 'YUV':
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2YUV)
+        frames_acc += frame
         # print('frame: ', currentFrame)
 
     # Compute mean and variance
-    # print('Computing mean and variance...')
+    print('Computing mean and variance...')
     meanImage = frames_acc / trainingFrames
 
     # set the current frame to 0 and compute the variance
     capture.set(cv2.CAP_PROP_POS_FRAMES, 0)
     currentFrame = int(capture.get(cv2.CAP_PROP_POS_FRAMES))
-    var_acc = np.zeros((height, width), dtype='float')
     success = True
     while success and currentFrame < trainingFrames:
         success, frame = capture.read()
         currentFrame = int(capture.get(cv2.CAP_PROP_POS_FRAMES))
-        grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        var_acc += (grayFrame - meanImage) ** 2
+        if colorspace == 'GRAY':
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        elif colorspace == 'YUV':
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2YUV)
+        var_acc += (frame - meanImage) ** 2
 
     varianceImage = (var_acc / trainingFrames)
 
@@ -52,29 +66,47 @@ def adaptive_bg_estimation(path, alpha=2.5, rho=0.1, mask_roi=None, path_to_save
     masks = []
     detections = dict()
     while success:
-        success, frame = capture.read()
+        success, srcFrame = capture.read()
         if not success: continue
         currentFrame = int(capture.get(cv2.CAP_PROP_POS_FRAMES))
-        grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # print('frame: ', currentFrame)
+        print('frame: ', currentFrame)
 
-        mask = np.absolute(grayFrame - meanImage) >= alpha * (np.sqrt(varianceImage) + 2)
-        if mask_roi is not None:
-            mask = mask & mask_roi
+        frame = srcFrame
+        if colorspace == 'GRAY':
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        elif colorspace == 'YUV':
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2YUV)
+
+        probabilityImage = np.absolute(frame - meanImage) - alpha * (np.sqrt(varianceImage)+ 2)
+        retVal, foregroundMask = cv2.threshold(probabilityImage, 0, 255, cv2.THRESH_BINARY)
+        foregroundMask = np.uint8(foregroundMask)
+        # cv2.imshow("window", foregroundMask)
+        # cv2.waitKey()
+
+        if colorspace != 'GRAY':
+            maskYU = cv2.bitwise_and(foregroundMask[:,:,0], foregroundMask[:,:,1])
+            maskYV = cv2.bitwise_and(foregroundMask[:,:,0], foregroundMask[:,:,2])
+            foregroundMask = cv2.bitwise_or(maskYU[:,:], maskYV[:,:])
+            if mask_roi is not None:
+                foregroundMask = cv2.bitwise_and(foregroundMask[:,:], mask_roi[:,:])
+        elif mask_roi is not None:
+            foregroundMask = cv2.bitwise_and(foregroundMask[:,:], mask_roi[:,:])
 
         if path_to_save is not None:
             if not os.path.exists(path_to_save):
                 os.makedirs(path_to_save)
-            cv2.imwrite(os.path.join(path_to_save, str(currentFrame) + '.png'), mask.astype('uint8') * 255)
+            cv2.imwrite(os.path.join(path_to_save, str(currentFrame) + '.png'), foregroundMask.astype('uint8'))
 
         # cv2.imshow("window", foregroundMask)
         # cv2.waitKey()
 
-        meanImage = (1-rho)*meanImage + rho*grayFrame
-        varianceImage = np.sqrt((1-rho)*np.power(varianceImage, 2) + rho*np.power((grayFrame-meanImage), 2))
+        meanImage = (1-rho)*meanImage + rho*frame
+        varianceImage = np.sqrt((1-rho)*np.power(varianceImage, 2) + rho*np.power((frame-meanImage), 2))
 
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-        maskResized = cv2.resize(mask, (0, 0), fx=0.4, fy=0.4)
+        # cv2.imshow("window", foregroundMask)
+        # cv2.waitKey()
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
+        maskResized = cv2.resize(foregroundMask, (0, 0), fx=0.4, fy=0.4)
         # maskResized = foregroundMask
         # cv2.imshow("window", maskResized)
         # cv2.waitKey()
@@ -83,19 +115,20 @@ def adaptive_bg_estimation(path, alpha=2.5, rho=0.1, mask_roi=None, path_to_save
         # cv2.imshow("window", openedMask)
         # cv2.waitKey()
         # openedMask = foregroundMask
-        openedMask = np.uint8(openedMask)*255
+        openedMask = np.uint8(openedMask)
 
         nlabels, labels, stats, centroids = cv2.connectedComponentsWithStats(openedMask, 4, cv2.CV_32S)
 
-        min_thresh = 2000
-        max_thresh = 1000000
+        min_size = 1000
+        max_size = 1000000
         for i in range(nlabels):
-            if stats[i][4] >= min_thresh and stats[i][4] <= max_thresh:
+            if stats[i][4] >= min_size and stats[i][4] <= max_size and (
+                    stats[i, cv2.CC_STAT_WIDTH] / stats[i, cv2.CC_STAT_HEIGHT]) < 2.8:
                 bbox = [stats[i, cv2.CC_STAT_LEFT],
                         stats[i, cv2.CC_STAT_TOP],
                         stats[i, cv2.CC_STAT_WIDTH],
                         stats[i, cv2.CC_STAT_HEIGHT]]
-                # cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[0] + bbox[2], bbox[1] + bbox[3]), (0, 0, 255), 7)
+                cv2.rectangle(srcFrame, (bbox[0], bbox[1]), (bbox[0] + bbox[2], bbox[1] + bbox[3]), (0, 0, 255), 7)
 
                 # add the detections in the dictionary
                 content = bbox
@@ -105,10 +138,11 @@ def adaptive_bg_estimation(path, alpha=2.5, rho=0.1, mask_roi=None, path_to_save
                 else:
                     detections[str(currentFrame)] = [content]
 
-        # plt.imshow(cv2.resize(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), (0,0), fx=rescaling_factor, fy=rescaling_factor))
-        # plt.show(block=False)
-        # plt.pause(0.05)
-        # plt.clf()
+        plt.imshow(
+            cv2.resize(cv2.cvtColor(srcFrame, cv2.COLOR_BGR2RGB), (0, 0), fx=rescaling_factor, fy=rescaling_factor))
+        plt.show(block=False)
+        plt.pause(0.05)
+        plt.clf()
 
         # plt.imshow(cv2.resize(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), (0,0), fx=rescaling_factor, fy=rescaling_factor))
 
@@ -123,7 +157,8 @@ def main(alpha, rho):
 
     mask_roi = cv2.imread(path_roi, cv2.IMREAD_GRAYSCALE)
 
-    bboxes_detected, trainingFrames = adaptive_bg_estimation(path, alpha=alpha, rho=rho, mask_roi=mask_roi, path_to_save=None)
+    bboxes_detected, trainingFrames = adaptive_bg_estimation(path, alpha=alpha, rho=rho, mask_roi=mask_roi, path_to_save=None
+                                                             ,colorspace='YUV')
 
     bboxes_gt, num_instances_gt = get_gt_bboxes()
     # get the number of instances in the validation split (needed to calculate the number of FN and the recall)
