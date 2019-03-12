76c9b9709ea5ced48c68b15f063ce194c637915e
failden <grzegorzskorupko@gmail.com>
2019-03-12 19:19:33 +0100

    add 3-channel support

--- code/week2/bg_estimation.py
+++ code/week2/bg_estimation.py
@@ -7,7 +7,7 @@ from utils.gt import get_gt_bboxes
 from utils.metrics import evaluation_detections, compute_map
 from utils.plot import plot_pr, show_bboxes
 
-def bgEstimate(path):
+def bgEstimate(path, colorspace='GRAY'):
     """
     :param path:
     :param bboxes:
@@ -26,40 +26,65 @@ def bgEstimate(path):
 
     # Load training sequence
 
-    trainingFrames = totalFrames * 0.25
+    trainingFrames = totalFrames * 0.10
     sequence = []
     meanImage = np.zeros((height, width), dtype='float')
     varianceImage = np.zeros((height, width), dtype='float')
     currentFrame = 0
     while success and currentFrame < trainingFrames:
-        success, frame = capture.read()
+        success, srcFrame = capture.read()
         currentFrame = int(capture.get(cv2.CAP_PROP_POS_FRAMES))
-        grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        sequence.append(grayFrame)
+
+        frame = srcFrame
+        if colorspace == 'GRAY':
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        elif colorspace == 'YUV':
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2YUV)
+
+        sequence.append(frame)
         print('frame: ', currentFrame)
 
     # Compute mean and variance
     print('Computing mean and variance...')
-    meanImage = np.mean(sequence, axis=0)
-    varianceImage = np.var(sequence, axis=0)
+    if colorspace == 'GRAY':
+        meanImage = np.mean(sequence, axis=0)
+        varianceImage = np.var(sequence, axis=0)
+    else:
+        meanImage = np.mean(sequence, axis=0)
+        # varianceImage = []
+        # for i in sequence
+        varianceImage = np.var(sequence, axis=0)
 
     # Extract foreground from remaining frames
-    alpha = 7
+    alpha = 2
     print(meanImage.shape)
     # plt.imshow(cv2.resize(varianceImage, (0,0), fx=rescaling_factor, fy=rescaling_factor))
     detections = dict()
     while success:
-        success, frame = capture.read()
+        success, srcFrame = capture.read()
         if not success:
             break
         currentFrame = int(capture.get(cv2.CAP_PROP_POS_FRAMES))
-        grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         print('frame: ', currentFrame)
-        probabilityImage = np.absolute(grayFrame - meanImage) - alpha * (np.sqrt(varianceImage)+ 2)
+
+        frame = srcFrame
+        if colorspace == 'GRAY':
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        elif colorspace == 'YUV':
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2YUV)
+
+        probabilityImage = np.absolute(frame - meanImage) - alpha * (np.sqrt(varianceImage)+ 2)
         retVal, foregroundMask = cv2.threshold(probabilityImage, 0, 255, cv2.THRESH_BINARY)
         # cv2.imshow("window", foregroundMask)
+        # cv2.waitKey()
+        if colorspace != 'GRAY':        
+            maskYU = cv2.bitwise_and(foregroundMask[:,:,0], foregroundMask[:,:,1])
+            maskYV = cv2.bitwise_and(foregroundMask[:,:,0], foregroundMask[:,:,2])
+            foregroundMask = cv2.bitwise_or(maskYU[:,:], maskYV[:,:])
+
+        # cv2.imshow("window", foregroundMask)
         # cv2.waitKey()        
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
         maskResized = cv2.resize(foregroundMask,(0,0), fx=0.4, fy=0.4)
         # maskResized = foregroundMask
         # cv2.imshow("window", maskResized)
@@ -73,15 +98,15 @@ def bgEstimate(path):
 
         nlabels, labels, stats, centroids = cv2.connectedComponentsWithStats(openedMask, 4, cv2.CV_32S)
 
-        min_thresh = 2000
-        max_thresh = 1000000
+        min_size = 1000
+        max_size = 1000000
         for i in range(nlabels):
-            if stats[i][4] >= min_thresh and stats[i][4] <= max_thresh:
+            if stats[i][4] >= min_size and stats[i][4] <= max_size and (stats[i, cv2.CC_STAT_WIDTH] / stats[i, cv2.CC_STAT_HEIGHT]) < 2.8 :
                 bbox = [stats[i, cv2.CC_STAT_LEFT],
                                     stats[i, cv2.CC_STAT_TOP], 
                                     stats[i, cv2.CC_STAT_WIDTH], 
                                     stats[i, cv2.CC_STAT_HEIGHT]]
-                cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[0] + bbox[2], bbox[1] + bbox[3]), (0,0,255), 7)
+                cv2.rectangle(srcFrame, (bbox[0], bbox[1]), (bbox[0] + bbox[2], bbox[1] + bbox[3]), (0,0,255), 7)
 
                 # add the detections in the dictionary
                 content = bbox
@@ -91,10 +116,10 @@ def bgEstimate(path):
                 else:
                     detections[str(currentFrame)] = [content]
 
-        # plt.imshow(cv2.resize(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), (0,0), fx=rescaling_factor, fy=rescaling_factor))
-        # plt.show(block=False)
-        # plt.pause(0.05)
-        # plt.clf()
+        plt.imshow(cv2.resize(cv2.cvtColor(srcFrame, cv2.COLOR_BGR2RGB), (0,0), fx=rescaling_factor, fy=rescaling_factor))
+        plt.show(block=False)
+        plt.pause(0.05)
+        plt.clf()
 
     # plt.imshow(cv2.resize(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), (0,0), fx=rescaling_factor, fy=rescaling_factor))
     
@@ -105,7 +130,7 @@ def bgEstimate(path):
 def main():
 
     path = '../../datasets/AICity_data/train/S03/c010/vdo.avi'
-    bboxes_detected, trainingFrames = bgEstimate(path)
+    bboxes_detected, trainingFrames = bgEstimate(path, 'YUV')
 
     bboxes_gt, num_instances_gt = get_gt_bboxes()
     # get the number of instances in the validation split (needed to calculate the number of FN and the recall)
