c01cd04b7767a149a3b6cc42510a7043124f7fee
manurare <mreyarea@gmail.com>
2019-03-19 11:31:26 +0100

    Adding tracking by overlap

---
+++ week3/track_overlap.py
@@ -0,0 +1,107 @@
+import os
+import sys
+import random
+import math
+import re
+import time
+import numpy as np
+import cv2
+import matplotlib.cm as cm
+import matplotlib.colors as colors
+import argparse
+import pickle
+import copy
+from matplotlib import colors
+from utils import bbox_iou
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--labels_dir', type=str, default='rcnn_detections_before_tuning/', help='')
+parser.add_argument('--video_dir', type=str, default='../datasets/AICity_data/train/S03/c010/vdo.avi',help='')
+
+def overlap_tracking(sorted_detections, threshold = 0.5):
+
+    assigned_id = 0
+    track_dict = dict()
+    whole_detections = []
+
+    for key, value in sorted_detections.items():  # value[0] = bboxes, value[1] = scores, value[2] = ids
+        current_frame_detections = []
+        if int(key) != 1:
+            for n_det, bbox in enumerate(value[0]):
+                scores_and_ids = [bbox_iou(bbox, prev_frame_bbox) for prev_frame_bbox in whole_detections[int(key)-2]]
+                max_score = max(scores_and_ids)[0]
+                track_id = max(scores_and_ids)[1]
+                if max_score >= threshold:
+                    current_frame_detections.append([track_id, bbox])
+                else:
+                    current_frame_detections.append([assigned_id, bbox])
+                    assigned_id += 1
+            whole_detections.append(current_frame_detections)
+        else:
+            for n_det, bbox in enumerate(value[0]):
+                current_frame_detections.append([assigned_id, bbox])
+                assigned_id += 1
+            whole_detections.append(current_frame_detections)
+
+    return whole_detections, assigned_id
+
+
+def show_tracked_detections(tracked_detections, max_id, video_dir):
+    capture = cv2.VideoCapture(video_dir)
+    frame_idx = 0
+
+    color_array = ["blue","black","brown","red","yellow","green","orange","beige","turquoise","pink"]
+
+    dict_colors = dict()
+
+    while True:
+        success, frame = capture.read()
+        if not success:
+            break
+        frame_idx += 1
+
+        for ind_detection in tracked_detections[frame_idx-1]:
+            if ind_detection[0] not in dict_colors.keys():
+                random.shuffle(color_array)
+                random_color = colors.to_rgb(color_array[0])
+                random_color = tuple([255 * x for x in random_color])
+                dict_colors[ind_detection[0]] = random_color
+
+            frame = cv2.rectangle(frame, (ind_detection[1][1], ind_detection[1][0]),
+                                  (ind_detection[1][3], ind_detection[1][2]), dict_colors[ind_detection[0]], 3)
+            frame = cv2.putText(frame, str(ind_detection[0]), (ind_detection[1][1]-5, ind_detection[1][0]-5),
+                                cv2.FONT_HERSHEY_COMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+            
+        cv2.namedWindow('output',cv2.WINDOW_NORMAL)
+        cv2.imshow('output', frame)
+        cv2.waitKey()
+
+
+
+if __name__ == "__main__":
+    """
+    Load the labels (rois, scores, ids) for each frame
+    and order them temporally
+    """
+
+    args, _ = parser.parse_known_args()
+    path_labels = args.labels_dir
+    path_video = args.video_dir
+
+    detections_dict = dict()
+    for filename in os.listdir(path_labels):
+        detections = pickle.load(open(path_labels + filename, "rb"))
+        frame_idx = filename.split(".")[0]
+        if frame_idx in detections_dict.keys():
+            detections_dict[frame_idx].append(detections)
+        else:
+            detections_dict[frame_idx] = detections
+
+    sorted_by_value = sorted(detections_dict.items(), key=lambda kv: int(kv[0]))
+
+    sorted_detections = dict()
+    for element in sorted_by_value:
+        sorted_detections[element[0]] = element[1]
+
+    tracked_detections, max_id = overlap_tracking(sorted_detections)
+    show_tracked_detections(tracked_detections, max_id, path_video)
---
+++ week3/utils.py
@@ -0,0 +1,39 @@
+import copy
+
+def bbox_iou(src_bboxA, src_bboxB):
+    # compute the intersection over union of two bboxes
+    # I've adapted this code from the M1 base code. The function expects [tly, tlx, width, height],
+    # where tl indicates the top left corner of the box.
+    bboxA = copy.deepcopy(src_bboxA)
+    bboxB = copy.deepcopy(src_bboxB)
+
+    id = 0
+    if len(bboxB) == 2:
+        id = bboxB[0]
+        bboxB = bboxB[1]
+
+
+    bboxA[2] = bboxA[0] + bboxA[2]
+    bboxA[3] = bboxA[1] + bboxA[3]
+    bboxB[2] = bboxB[0] + bboxB[2]
+    bboxB[3] = bboxB[1] + bboxB[3]
+
+    xA = max(bboxA[1], bboxB[1])
+    yA = max(bboxA[0], bboxB[0])
+    xB = min(bboxA[3], bboxB[3])
+    yB = min(bboxA[2], bboxB[2])
+
+    # compute the area of intersection rectangle
+    interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+
+    # compute the area of both bboxes
+    bboxAArea = (bboxA[2] - bboxA[0] + 1) * (bboxA[3] - bboxA[1] + 1)
+    bboxBArea = (bboxB[2] - bboxB[0] + 1) * (bboxB[3] - bboxB[1] + 1)
+
+    try:
+        iou = interArea / float(bboxAArea + bboxBArea - interArea)
+    except ZeroDivisionError:
+        iou = 0.0
+
+    # return the intersection over union value
+    return iou, id
