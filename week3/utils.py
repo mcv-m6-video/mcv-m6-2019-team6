f413e6c15f34092811966bebc159017e6c09e857
failden <grzegorzskorupko@gmail.com>
2019-03-19 18:18:29 +0100

    add reading annotations

--- week3/utils.py
+++ week3/utils.py
@@ -1,5 +1,10 @@
 import copy
 
+from random import randrange, random
+import numpy as np
+from xml.dom import minidom
+import os
+
 def bbox_iou(src_bboxA, src_bboxB):
     # compute the intersection over union of two bboxes
     # I've adapted this code from the M1 base code. The function expects [tly, tlx, width, height],
@@ -37,3 +42,25 @@ def bbox_iou(src_bboxA, src_bboxB):
 
     # return the intersection over union value
     return iou, id
+
+def read_xml_annotations(annotations_path):
+    files = os.listdir(annotations_path)
+
+    bboxes = dict()
+    for file_ in files:
+        xmldoc = minidom.parse(annotations_path + file_)
+        bboxes_list = xmldoc.getElementsByTagName('box')
+        for element in bboxes_list:
+            frame = element.getAttribute('frame')
+            xtl = int(float(element.getAttribute('xtl')))
+            ytl = int(float(element.getAttribute('ytl')))
+            xbr = int(float(element.getAttribute('xbr')))
+            ybr = int(float(element.getAttribute('ybr')))
+            width = ybr - ytl
+            height = xbr - xtl
+
+            if frame in bboxes.keys():
+                bboxes[frame].append(['car', xtl, ytl, height, width, random()])
+            else:
+                bboxes[frame] = [['car', xtl, ytl, height, width, random()]]
+    return bboxes
